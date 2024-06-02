599f001a82bee31b5fd1ca702820b5d3767c5039
Otipa <otipajames2@gmail.com>
2024-06-02 06:14:01 +0300

    added attributes

--- config/config.php
+++ config/config.php
@@ -13,3 +13,11 @@ define("USER", "root");
 //password
 define("PASS", "");
 
+$conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME, USER, PASS);
+
+// Check connection
+if ($conn == true){
+    echo "Connected successfully";
+} else {
+    echo "Connection failed: ";
+}
