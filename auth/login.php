929b13f98d99d346dbf289725864ea530f23d011
Otipa <otipajames2@gmail.com>
2024-06-05 13:12:20 +0300

    modified login.php and register.php

--- auth/login.php
+++ auth/login.php
@@ -13,6 +13,28 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+
+    //Write a query check for the email
+    //changes made from login uders in tutorial at 10min 40seconds
+
+    $login = $conn->query("SELECT * FROM users WHERE email = :email");
+    $login->execute();
+
+    $fetch = $login->fetch(PDO::FETCH_ASSOC);
+
+    if($login->rowCount() > 0){
+      if(password_verify($password, $fetch['password'])){
+        // $_SESSION['email'] = $fetch['email'];
+        // $_SESSION['username'] = $fetch['username'];
+        // $_SESSION['id'] = $fetch['id'];
+        header("Location: " .APPURL. "");
+      } else {
+        echo "<script>alert('Invalid email or password');</script>";
+      }
+    } else {
+      echo "<script>alert('Invalid email or password');</script>";
+    }
+
   } }
 
 ?>
@@ -37,27 +59,27 @@ if(isset($_POST['submit'])){
       <div class="container">
         <div class="row">
           <div class="col-md-12 ftco-animate">
-			<form action="#" class="billing-form ftco-bg-dark p-3 p-md-5">
+			<form action="login.php" method="POST" class="billing-form ftco-bg-dark p-3 p-md-5">
 				<h3 class="mb-4 billing-heading">Login</h3>
 	          	<div class="row align-items-end">
 	          		<div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Email">Email</label>
-	                  <input type="text" class="form-control" placeholder="Email">
+	                  <input name="email" type="text" class="form-control" placeholder="Email">
 	                </div>
 	              </div>
                  
 	              <div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Password">Password</label>
-	                    <input type="password" class="form-control" placeholder="Password">
+	                    <input name="password" type="password" class="form-control" placeholder="Password">
 	                </div>
 
                 </div>
                 <div class="col-md-12">
                 	<div class="form-group mt-4">
 							<div class="radio">
-                                <button class="btn btn-primary py-3 px-4">Login</button>
+          <button name="submit" type="submit" class="btn btn-primary py-3 px-4">Login</button>
 						    </div>
 					</div>
                 </div>
